4ffeaf9ca77040f39398e1377680da78e181f8e3
Henry Galvez <soulofhenry@gmail.com>
2020-09-20 19:29:49 -0600

    se cambio la ñ por ni

--- src/test/java/Proceso/HenryGalvez_201612499.java
+++ src/test/java/Proceso/HenryGalvez_201612499.java
@@ -7,15 +7,39 @@ package Proceso;
 
 import Objetos.Carrera;
 import Objetos.RegistroAcademico;
+import Objetos.Horario;
 import org.junit.jupiter.api.*;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.BDDMockito.given;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import static org.mockito.Mockito.when;
 import org.mockito.MockitoAnnotations;
 import org.mockito.Spy;
 
-public class PlantillaParaTestNoEditar {
-
+public class HenryGalvez_201612499 {
+    
+    AsignarHorario ah = null;
+    Carrera c = null;
+    Horario h = null;
+    RegistroAcademico ra = null;
+    
+    
+    @Test
+    public void testGenerarHorario() throws Exception{
+        Carrera carr = new Carrera(9, "la mejor carrera del mundo :v");
+        int cod = carr.getCodigo();
+        assertEquals(9, cod);
+    }
+    
+     @Test
+    public void testGenerarHorarioMock() throws Exception{
+        c = new Carrera(9, "la mejor carrera del mundo :v");
+        ra = new RegistroAcademico(2016,"12499");
+        ah = Mockito.mock(AsignarHorario.class);
+        when(ah.generarHorario(c, ra)).thenReturn(new Horario(4,"Horario interceptado"));
+        h = ah.generarHorario(c, ra);
+        assertEquals(h.getDescripcion(),"Horario interceptado");
+    }
 }
